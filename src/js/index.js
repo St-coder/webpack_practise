d992602f20df66908622b769b358adae61beab37
gonghai <gong__hai@126.com>
2020-10-10 16:59:53 +0800

    生产环境的基本配置

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,12 @@
-import '../css/index.less'
-import '../assets/README.md'
-function add (x, y) {
-    return x+y
+import '../css/index.less';
+import '../assets/README.md';
+
+function add(x, y) {
+  return x + y;
 }
-console.log(add(1,2))
+
+// eslint-disable-next-line
+console.log(add(1, 2));
+
+/* eslint-disable */
+/* eslint-disable */
--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,128 @@
-const {resolve} = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-module.exports = {
-    entry: './src/js/index.js',
-    output: {
-        filename:'js/built.js',
-        path: resolve(__dirname, 'build')
-    },
-    module: {
-        rules: [
-            // 处理less资源
-            {
-                test: /\.less$/,
-                use: ['style-loader', 'css-loader', 'less-loader']
-            },
-            // 处理css资源
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            // 处理图片资源
-            {
-                // 此loader处理不了样式中的图片资源,需html-loader才能处理
-                test: /\.(jpg|png|gif|jpeg)$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 8* 1024,
-                    name: '[hash:10].[ext]',
-                    esModule: false,
-                    outputPath: 'imgs',
-                }
-            },
-            // 处理html中的图片资源
-            {
-                test: /\.html$/,
-                loader: 'html-loader'
-                //html-loader对图片资源 用commonjs的方式引入， 
-                // 而url-loader会通过esModule去解析，两个模块化不一样，就会导致解析失败。
-                //所以需要关闭url-loader的esModule，都以commonjs的方式解析
-            },
-            //处理其他资源
-            {
-                exclude: /\.(html|js|css|less|jpg|png|gif|jpeg)/,
-                loader: 'file-loader',
-                options: {
-                    name: '[hash:10].[ext]',
-                    outputPath: 'media'
-                    
-                }
-            }
+const { resolve } = require('path');
+// 处理html中的图片
+const HtmlWebpackPLugin = require('html-webpack-plugin');
+//  提取css
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// 压缩css
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+// 复用loader
+const CommonCssLoader = [
+  //  提取成单独的文件
+  MiniCssExtractPlugin.loader,
+  //  把css切入到js中
+  'css-loader',
+  // 做兼容性处理
+  // {
+  //     loader: 'postcss-loader',
+  //     options: {
+  //         ident: 'postcss',
+  //         plugins: () =>{
+  //             require('postcss-preset-env')()
+  //         }
+  //     }
+  // },
+];
 
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+process.env.NODE_ENV = 'production';
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          ...CommonCssLoader,
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          ...CommonCssLoader,
+          // 把less---> css
+          'less-loader',
+        ],
+      },
+      {
+        // 检查js
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        // 指定loader执行顺序
+        enforce: 'pre',
+        options: {
+          fix: true,
+        },
+      },
+      {
+        // 兼容js
+        // test: /\.js$/,
+        // exclude: /node_modules/,
+        // loader: 'babel-loader',
+        // options: {
+        //     presets: [
+        //         [
+        //             '@babel/preset-env',
+        //             {
+        //                 // 按需加载
+        //                 useBuiltIns: 'usage',
+        //                 corejs: {version: 3},
+        //                 targets: {
+        //                     chrome: '60',
+        //                     firefox: '50'
+        //                 }
+        //             }
+        //         ]
+        //     ]
+        // }
+      },
+      {
+        // 处理图片
+        test: /\.(jpg|png|jpeg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          name: '[hash:10].[ext]',
+          outputPath: 'imgs',
+          esModule: false,
+        },
+      },
+      {
+        // 处理html中的图片
+        test: /\.html$/,
+        loader: 'html-loader',
+        // options: {
+        //     limit: 8*1024,
+        //     name: '[hash:10].[ext]',
+        //     outputPath: 'imgs',
+        // }
+      },
+      {
+        // 处理其他文件
+        exclude: /\.(js|html|jpg|png|jpeg|less|css)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'assets',
+        },
+      },
     ],
-    devServer: {
-        contentBase: resolve(__dirname, 'build'),
-        // gzip压缩
-        compress: true,
-        port: 3000,
-        // 自动打开服务器
-        open: true,
-    },
-    mode: 'development'
-    
-}
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/built.css',
+    }),
+    new OptimizeCssAssetsWebpackPlugin(),
+    new HtmlWebpackPLugin({
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+  ],
+};
